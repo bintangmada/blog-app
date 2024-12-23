4d3e86438438215e1272d20e37acd121d6698fba
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2024-12-23 13:56:51 +0700

    versioning api by parameters

--- src/main/java/com/blog_app/controller/PostController.java
+++ src/main/java/com/blog_app/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.blog_app.controller;
 
 import com.blog_app.payload.PostDto;
+import com.blog_app.payload.PostDtoV2;
 import com.blog_app.payload.PostResponse;
 import com.blog_app.service.PostService;
 import com.blog_app.utils.AppConstans;
@@ -14,10 +15,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/post/v2")
 @Tag(
         name = "CRUD Rest APIs for Post Resource"
 )
@@ -64,9 +66,31 @@ public class PostController {
             responseCode = "200",
             description = "Http Status 201 OKE"
     )
-    @GetMapping("/getById/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable("id") Long id){
+
+    // versioning by parameters
+    @GetMapping(value = "/getById/{id}", params = "version=1")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable("id") Long id){
         return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // versioning by parameters
+    @GetMapping(value = "/getById/{id}", params = "version=2")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable("id") Long id){
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setContent(postDto.getContent());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setCategoryId(postDto.getCategoryId());
+        postDtoV2.setDeletedStatus(postDtoV2.getDeletedStatus());
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+
+        return ResponseEntity.ok(postDtoV2);
     }
 
     @SecurityRequirement(
