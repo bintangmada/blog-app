a6e1186fe995e480574723f8ad8a3be35cbb0e12
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2024-12-23 14:36:03 +0700

    versioning api by content negotiation

--- src/main/java/com/blog_app/controller/PostController.java
+++ src/main/java/com/blog_app/controller/PostController.java
@@ -66,15 +66,14 @@ public class PostController {
             responseCode = "200",
             description = "Http Status 201 OKE"
     )
-
-    // versioning api by headers
-    @GetMapping(value = "/getById/{id}", headers = "X-API-VERSION=1")
+    // versioning api by content negotiation
+    @GetMapping(value = "/getById/{id}", produces = "application/bintang.springboot.v1+json")
     public ResponseEntity<PostDto> getPostByIdV1(@PathVariable("id") Long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    // versioning api by headers
-    @GetMapping(value = "/getById/{id}", headers = "X-API-VERSION=2")
+    // versioning api by content negotiation
+    @GetMapping(value = "/getById/{id}", produces = "application/bintang.springboot.v2+json")
     public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable("id") Long id){
         PostDto postDto = postService.getPostById(id);
         PostDtoV2 postDtoV2 = new PostDtoV2();
@@ -93,13 +92,39 @@ public class PostController {
         return ResponseEntity.ok(postDtoV2);
     }
 
-    // versioning by parameters
+//    // versioning api by headers
+//    @GetMapping(value = "/getById/{id}", headers = "X-API-VERSION=1")
+//    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable("id") Long id){
+//        return ResponseEntity.ok(postService.getPostById(id));
+//    }
+//
+//    // versioning api by headers
+//    @GetMapping(value = "/getById/{id}", headers = "X-API-VERSION=2")
+//    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable("id") Long id){
+//        PostDto postDto = postService.getPostById(id);
+//        PostDtoV2 postDtoV2 = new PostDtoV2();
+//        postDtoV2.setId(postDto.getId());
+//        postDtoV2.setTitle(postDto.getTitle());
+//        postDtoV2.setContent(postDto.getContent());
+//        postDtoV2.setDescription(postDto.getDescription());
+//        postDtoV2.setCategoryId(postDto.getCategoryId());
+//        postDtoV2.setDeletedStatus(postDto.getDeletedStatus());
+//        List<String> tags = new ArrayList<>();
+//        tags.add("Java");
+//        tags.add("Spring Boot");
+//        tags.add("AWS");
+//        postDtoV2.setTags(tags);
+//
+//        return ResponseEntity.ok(postDtoV2);
+//    }
+
+//    versioning by parameters
 //    @GetMapping(value = "/getById/{id}", params = "version=1")
 //    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable("id") Long id){
 //        return ResponseEntity.ok(postService.getPostById(id));
 //    }
 
-    // versioning by parameters
+//    versioning by parameters
 //    @GetMapping(value = "/getById/{id}", params = "version=2")
 //    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable("id") Long id){
 //        PostDto postDto = postService.getPostById(id);
